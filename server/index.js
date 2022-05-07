1cdf87689dfcce16ca1d9e7e9e5d854a97544dec
aronmakiika <aronmakiika@gmail.com>
2022-05-07 20:26:48 +0300

    chore: add swagger jsdoc options

--- server/index.js
+++ server/index.js
@@ -3,14 +3,26 @@ const app = express();
 const cors = require("cors");
 const db = require("./models");
 const newsRouter = require("./routes/news");
+const swaggerJsdoc = require("swagger-jsdoc");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-
+const swaggerOptions = {
+  swaggerDefinition: {
+    info: {
+      title: "News app API",
+      version: "1.0.0",
+    },
+  },
+  apis: ["./routes/news.js"],
+};
 // app.use("/images", express.static("./images"));
 //routes
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+
 app.use("/news", newsRouter);
 
 db.sequelize.sync().then(() => {
